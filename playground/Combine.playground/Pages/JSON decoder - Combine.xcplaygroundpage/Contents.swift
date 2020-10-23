97cd030ebcedee4ec28a1af17b3947dadb159889
primecoder <Ace1Authors@gmail.com>
2020-10-23 18:03:14 +1100

    More study on Combine

--- playground/Combine.playground/Pages/JSON decoder - Combine.xcplaygroundpage/Contents.swift
+++ playground/Combine.playground/Pages/JSON decoder - Combine.xcplaygroundpage/Contents.swift
@@ -1,13 +1,5 @@
-/// Codable JSON data - no Combine
-///
-/// In this playground, I had a look at how Combine can be used
-/// in the pattern of read-translate-serialise data into objects.
-///
-
 import Foundation
 import Combine
-
-// MARK: -
 
 // Get the data from resource bundle.
 guard let dataFile = Bundle.main.path(forResource: "reviews", ofType: "json") else {
@@ -17,9 +9,14 @@ guard let dataFile = Bundle.main.path(forResource: "reviews", ofType: "json") el
 
 do {
     let data = try Data(contentsOf: URL(fileURLWithPath: dataFile))
-    try JSONDecoder()
-        .decode(Reviews.self, from: data)
-        .map { print("Review: \($0.text), label: \($0.label)") }
+    Just(data)
+        .decode(type: Reviews.self, decoder: JSONDecoder())
+        .sink { (completion) in
+            print("Completed")
+        } receiveValue: { (reviews) in
+            _ = reviews.map { print("Review: \($0.text), label: \($0.label)") }
+        }
+
 } catch {
     print("ERROR> \(error)")
 }
---
+++ playground/Combine.playground/Sources/Reviews.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+// Put in top-level Sources folder to share between pages.
+// But, damn!, to make my Xcode Playground recompile, I had to quit and restart Xcode
+// each time!!! :-(
+// [aa-20201023]
+
+// MARK: - Review
+// Created using app.quicktype.io
+public struct Review: Codable {
+    public let text: String
+    public let label: Label
+}
+
+public enum Label: String, Codable {
+    case negative = "negative"
+    case neutral = "neutral"
+    case positive = "positive"
+}
+
+public typealias Reviews = [Review]
