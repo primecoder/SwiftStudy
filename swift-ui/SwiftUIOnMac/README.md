66600a31a8a60d09a61adf160d18e7c1a49b9fe0
primecoder <Ace1Authors@gmail.com>
2020-10-31 14:33:17 +1100

    Studying how to do mac app with SwiftUI

---
+++ swift-ui/SwiftUIOnMac/README.md
@@ -0,0 +1,9 @@
+#  SwiftUI on Mac
+
+This is to study implementing Mac OS apps using SwiftUI framework.
+
+Xcode project 'SwiftUIOnMac' represents the simplest form of creating Mac app using
+SwiftUI.
+
+
+Author: Ace Authors
---
+++ swift-ui/SwiftUIOnMac/SwiftUIOnMac/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  SwiftUIOnMac
+//
+//  Created by random on 31/10/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
