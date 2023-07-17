d3c495e3dcb998cdf6da08b4ff4b5722447a8dd8
Ace <Ace1Authors@gmail.com>
2023-07-17 17:53:06 +1000

    Added more examples for Combine and SwiftUI on Playground

---
+++ playground/Combine.playground/Pages/Timer + SwiftUI.xcplaygroundpage/Contents.swift
@@ -0,0 +1,95 @@
+
+import SwiftUI
+import PlaygroundSupport    // Need to import this one.
+import Combine
+
+struct DigitalClock: View {
+    
+    @ObservedObject var dataModel: DataModel
+    
+    var body: some View {
+        VStack {
+            Text("Time: \(dataModel.timeHHMMSS)")
+                .font(.title)
+            Divider()
+            HStack {
+                Text("Time: \(dataModel.timeHHMM)")
+                    .font(.largeTitle)
+                ZStack {
+                    ProgressView(value: Double(dataModel.seconds), total: 60)
+                        .progressViewStyle(.circular)
+                    Text("\(dataModel.seconds)")
+                }
+                .padding()
+            }
+        }
+        .padding()
+        .frame(width: 300)
+        .background(Color.white)
+        .foregroundColor(Color.black)
+        .cornerRadius(15)
+        .padding()
+    }
+}
+
+// This will display SwiftUI contents.
+PlaygroundPage.current.setLiveView(
+    DigitalClock(
+        dataModel: DataModel().start()
+    )
+)
+
+// This DataModel demonstrates how to encapsulate (hide) all implementation details.
+// It hides the uses of Combine Framework inside.
+// The caller (SwiftUI view) only needs to concern about updatable object and how to
+// display it.
+// [aa-20230717]
+class DataModel: ObservableObject {
+    @Published var timeHHMMSS: String = "--:--:--"
+    @Published var timeHHMM: String = "--:--"
+    @Published var seconds: Int = 0
+    
+    private static let dateFormatter = {
+        let fmt = DateFormatter()
+        fmt.dateFormat = "HH:mm:ss"
+        return fmt
+    }()
+    
+    private static let dateFormatterHHMM = {
+        let fmt = DateFormatter()
+        fmt.dateFormat = "HH:mm"
+        return fmt
+    }()
+    
+    private var timer = Timer.publish(every: 1, on: .main, in: .default)
+    private var cancellable1: AnyCancellable?
+    private var cancellable2: AnyCancellable?
+    private var cancellable3: AnyCancellable?
+    
+    func start() -> DataModel {
+        if (cancellable1 == nil) {
+            cancellable1 = timer
+            //        .autoconnect()
+                .map { DataModel.dateFormatter.string(from: $0) }
+                .assign(to: \.timeHHMMSS, on: self)       // Example of how to use `assign(to:)`
+            //        .sink { self.time = $0 }
+        }
+        
+        if (cancellable2 == nil) {
+            cancellable2 = timer
+                .map { DataModel.dateFormatterHHMM.string(from: $0) }
+                .assign(to: \.timeHHMM, on: self)
+        }
+        
+        if (cancellable3 == nil) {
+            cancellable3 = timer
+                .map { Int($0.timeIntervalSince1970) % 60 }
+                .assign(to: \.seconds, on: self)
+        }
+        
+        // This shows an example of connecting manually
+        timer.connect()
+        
+        return self
+    }
+}
--- playground/Combine.playground/Pages/Timer using Combine.xcplaygroundpage/Contents.swift
+++ playground/Combine.playground/Pages/Timer using Combine.xcplaygroundpage/Contents.swift
@@ -8,8 +8,8 @@ import Foundation
 import Combine
 
 // Simplest timer using Combine.
-let cancellable = Timer.publish(every: 1, on: .main, in: .default)
-    .autoconnect()
+let cancellable1 = Timer.publish(every: 1, on: .main, in: .default)
+    .autoconnect()  // Auto connect to timer and run straight away
     .sink { time in
         print("1> \(time)")
     }
@@ -18,21 +18,54 @@ let cancellable = Timer.publish(every: 1, on: .main, in: .default)
 // NOTE:
 // - Need to store the value returned by 'sink' (in cancellable2) to keep it in scope,
 //   or else the timer will not run.
-var repeatCount = 0
 let timer = Timer.publish(every: 1, on: .main, in: .default)
+
+var cancellable2Count = 0
 let cancellable2 = timer.sink(receiveCompletion: { _ in
     print("2> All done")
 }, receiveValue: { time in
-    repeatCount += 1
-    print("2> count=\(repeatCount), time: \(time)")
-    if repeatCount >= 5 {
-        timer.connect().cancel()    // Cancel the 2nd timer. Connect first then cancel.
-        cancellable.cancel()        // Cancel the first timer. See line 11
+    cancellable2Count += 1
+    print("2> count=\(cancellable2Count), time: \(time)")
+    if cancellable2Count >= 5 {
+        
+        // These 2 lines work the same.
+//        timer.connect().cancel()    // Cancel the 2nd timer. Connect first then cancel.
+        cancellable2.cancel()
+        
+        cancellable1.cancel()        // Cancel the first timer. See line 11
     }
 })
 
-// When not using autoconnect(), 
-// This will start the timer and run the code block above.
+var cancellable3Count = 0
+let cancellable3 = timer.sink { _ in
+    print("3> All done")
+} receiveValue: { time in
+    cancellable3Count += 1
+    print("3> count=\(cancellable3Count), time: \(time)")
+    if cancellable3Count >= 10 {
+        cancellable3.cancel()
+    }
+}
+
+let dateFormatter = DateFormatter()
+dateFormatter.dateFormat = "YYYY-MM-dd HH:ss"
+var cancellable4Count = 0
+let cancellable4 = timer
+    .map { dateFormatter.string(from: $0) }         // Using operator to convert time to string and republish as string.
+    .map { ">>>>> \($0) <<<<<"}                     // More conversion here
+    .sink { _ in
+        print("4> All done")
+    } receiveValue: { timeDesc in
+        cancellable4Count += 1
+        print("4> count=\(cancellable4Count), time: \(timeDesc)")
+        if cancellable4Count >= 3 {
+            cancellable4.cancel()
+        }
+    }
+
+
+// When not using autoconnect(), timer will not start publishing until manually connect.
+// This will start the timer and calls to all subscribers (cancellable2/3/4)
 timer.connect()
 
 print("All Done")
