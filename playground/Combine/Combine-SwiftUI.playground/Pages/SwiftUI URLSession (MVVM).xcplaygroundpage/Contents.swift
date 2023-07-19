2362cfa20189e186f324997b396b017613728ea3
Ace <Ace1Authors@gmail.com>
2023-07-19 13:34:31 +1000

    Tidy up

--- playground/Combine/Combine-SwiftUI.playground/Pages/SwiftUI URLSession (MVVM).xcplaygroundpage/Contents.swift
+++ playground/Combine/Combine-SwiftUI.playground/Pages/SwiftUI URLSession (MVVM).xcplaygroundpage/Contents.swift
@@ -21,7 +21,14 @@ struct PeopleView: View {
             HStack {
                 Text("Status:")
                 Text(viewModel.status.rawValue)
-                Text("(loaded \(viewModel.loadBytes) bytes)")
+                switch (viewModel.status) {
+                case .ready:
+                    Text("(loaded \(viewModel.loadBytes) bytes)")
+                case .loading:
+                    Text("(attempt #\(viewModel.attemptCount))")
+                default:
+                    Text("")
+                }
             }
             Divider()
             ScrollView {
@@ -33,7 +40,7 @@ struct PeopleView: View {
             }
             .overlay(viewModel.status == .loading ? ProgressView() : nil)
         }
-        .frame(width: 600, height: 400)
+        .frame(width: 400, height: 500)
     }
 }
 
@@ -42,26 +49,38 @@ struct PersonView: View {
     
     var body: some View {
         Text("\(person.name) \(person.surname)")
+            .padding(5)
+            .frame(maxWidth: .infinity)
+            .foregroundColor(Color.black)
+            .background(Color.white)
+            .cornerRadius(5)
     }
 }
 
 class ViewModel: ObservableObject {
     enum Status: String {
-        case initialising = "Initialising"
+        case initialised = "Initialised"
         case loading = "Loading..."
         case loadError = "Error"
         case ready = "Ready"
     }
     
-    @Published var status: Status = .initialising
+    @Published var status: Status = .initialised
     @Published var people: People = People()
     @Published var loadBytes: Int = 0
+    @Published var attemptCount: Int = 1
     
     private static let url = URL(string: "http://127.0.0.1:5000/unstable/persons")!
     
     private func makePublisher() -> AnyPublisher<Data, Error> {
+        attemptCount = 1
         return URLSession.shared
             .dataTaskPublisher(for: ViewModel.url)
+            .map {
+                print("attemp count: \(self.attemptCount)")
+                self.attemptCount += 1
+                return $0
+            }
             .tryMap({ element -> Data in
                 guard let httpResp = element.response as? HTTPURLResponse,
                       httpResp.statusCode == 200 else {
@@ -88,6 +107,7 @@ class ViewModel: ObservableObject {
     
     func loadWithTestData() {
         people = People.mockedPeople
+        status = .ready
     }
     
     func loadData() {
@@ -98,14 +118,15 @@ class ViewModel: ObservableObject {
         
         publisher
             .decode(type: People.self, decoder: JSONDecoder())
-            .sink { completion in
+            .replaceError(with: People(persons: []))
+            .sink { [self] completion in
                 switch (completion) {
                 case .finished:
                     self.status = .ready
                 case .failure(_):
                     self.status = .loadError
                 }
-            } receiveValue: { people in
+            } receiveValue: { [self] people in
                 self.people = people
             }
             .store(in: &cancellables)
@@ -113,7 +134,7 @@ class ViewModel: ObservableObject {
         publisher
             .sink { completion in
                 //
-            } receiveValue: { data in
+            } receiveValue: { [self] data in
                 print("Data: \(data.count)")
                 self.loadBytes = data.count
             }
